85138a44012de79d6b6078e9d0656d1ea5df1afe
f7 <yushenkor@yandex.ru>
2016-05-21 18:43:36 +0300

    Класс для SplashScreen При запуске программы с начала включается SplashScreen, а потом уже главнео окно программы

---
+++ CoursProj_TaskManager/fMain/SplashScreen.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace fMain
+{
+    public partial class SplashScreen : Form
+    {
+        public SplashScreen()
+        {
+            InitializeComponent();
+            Timer t = new Timer();
+            t.Interval = 6000;
+            t.Start();
+            t.Tick += new EventHandler(t_Tick);
+
+            timer1.Start();
+            Opacity = 0;
+            Timer timer = new Timer();
+            timer.Tick += new EventHandler((sender, e) =>
+            {
+                if ((Opacity += 0.005d) == 1) timer.Stop();
+            });
+            timer.Interval = 1;
+            timer.Start();
+        }
+        void t_Tick(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
